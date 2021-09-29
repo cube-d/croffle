c029021d8438587a83da5a85ee0d1c09a76438ec
vljh246v <vljh246v@naver.com>
2021-09-30 00:20:44 +0900

    create hello controller

---
+++ src/main/java/com/dcube/croffle/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.dcube.croffle.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello, world";
+    }
+}
