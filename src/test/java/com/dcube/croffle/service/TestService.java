648dcd3a33a92eb7f4c5f47f584fa042957dd678
vljh246v <vljh246v@naver.com>
2021-11-24 00:49:42 +0900

    [test] add test code

---
+++ src/test/java/com/dcube/croffle/service/TestService.java
@@ -0,0 +1,14 @@
+package com.dcube.croffle.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class TestService {
+
+  @Test
+  void test(){
+    assertThat(true).isFalse();
+  }
+
+}
